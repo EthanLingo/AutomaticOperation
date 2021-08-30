a25c0ae769df2e4c1809b5d4626ddf46a3915781
EthanSystem <ElementsPhoenix@outlook.com>
2021-08-31 00:55:36 +0800

    mod 2021-08-31 00:55:35

--- Projects/正则提取md文件内容输出为json.sh
+++
@@ -1,2 +0,0 @@
-cd /Users/ethan/LocalFiles/CodesFile/AO/AutomaticOperation
-sh Operation/MarkdownOperation/renameLinksAtMarkdownFiles.sh "^(.*)(\[\[\()(.*)\,\s(.*)(\)\]\])(.*)$" "{"author":"\3","year":"\4"}," "/Users/ethan/LocalFiles/CodesFile/AO/AutomaticOperation/data/mdfiles/original.txt" "/Users/ethan/LocalFiles/CodesFile/AO/AutomaticOperation/data/output/output_01.txt"
